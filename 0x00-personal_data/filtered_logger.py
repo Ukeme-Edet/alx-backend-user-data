12b9b089a24deed29f1af35a1f98d2c2ad791473
Ukeme Edet <ukemeedet2207@gmail.com>
2024-11-06 15:09:46 +0100

    Add filtered_logger.py module with filter_datum function

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+This module contains a method that returns the log message obfuscated
+"""
+import re
+from typing import List
+
+
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
+    """
+    Returns the log message obfuscated
+
+    Args:
+        fields (List[str]): a list of strings representing all fields to\
+            obfuscate
+        redaction (str): a string representing by what the field will be\
+            obfuscated
+        message (str): a string representing the log line
+        separator (str): a string representing by which character is\
+            separating all fields in the log line
+
+    Returns:
+        str: the log message obfuscated
+    """
+    for field in fields:
+        message = re.sub(
+            f"{field}=(.*?){separator}",
+            f"{field}={redaction}{separator}",
+            message,
+        )
+    return message
