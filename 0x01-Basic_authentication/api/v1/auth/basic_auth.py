022980ce546ebfdc1e8cab55c2a871a5b43b05a3
Ukeme Edet <ukemeedet2207@gmail.com>
2024-11-13 00:17:08 +0100

    Refactor BasicAuth class to include a method for extracting the base64 authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,23 @@ class BasicAuth(Auth):
     BasicAuth class
     """
 
-    pass
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> str:
+        """
+        extract_base64_authorization_header method
+
+        Args:
+            authorization_header (str): authorization_header
+
+        Returns:
+            str: None
+        """
+        if (
+            authorization_header is None
+            or type(authorization_header) is not str
+        ):
+            return None
+        if authorization_header[:6] != "Basic ":
+            return None
+        return authorization_header[6:]
