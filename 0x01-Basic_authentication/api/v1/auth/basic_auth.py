1e4d4d6105fc57f86e346e0f5849e4af20827ed4
Ukeme Edet <ukemeedet2207@gmail.com>
2024-11-13 00:27:21 +0100

    Refactor BasicAuth class to include a method for extracting user credentials from the decoded base64 authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -55,3 +55,23 @@ class BasicAuth(Auth):
             ).decode("utf-8")
         except Exception:
             return None
+
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str
+    ) -> (str, str):
+        """
+        extract_user_credentials method
+
+        Args:
+            decoded_base64_authorization_header (str): decoded_base64_authorization_header
+
+        Returns:
+            (str, str): None
+        """
+        if (
+            decoded_base64_authorization_header is None
+            or type(decoded_base64_authorization_header) is not str
+            or ":" not in decoded_base64_authorization_header
+        ):
+            return (None, None)
+        return tuple(decoded_base64_authorization_header.split(":", 1))
