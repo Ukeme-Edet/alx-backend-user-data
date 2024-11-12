f2d87301dc1135fa288facf9b9415d9f5ec62994
Ukeme Edet <ukemeedet2207@gmail.com>
2024-11-13 00:30:25 +0100

    Refactor BasicAuth class to include a method for extracting user credentials from the decoded base64 authorization header and a method for creating a user object from the extracted credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """
 Auth module
 """
+from typing import TypeVar
 from .auth import Auth
 import base64
 
@@ -76,3 +77,32 @@ class BasicAuth(Auth):
         ):
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(":", 1))
+
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+    ) -> TypeVar("User"):
+        """
+        user_object_from_credentials method
+
+        Args:
+            user_email (str): user_email
+            user_pwd (str): user_pwd
+
+        Returns:
+            TypeVar('User'): None
+        """
+        if (
+            user_email is None
+            or type(user_email) is not str
+            or user_pwd is None
+            or type(user_pwd) is not str
+        ):
+            return None
+        from models.user import User
+
+        user = User.search({"email": user_email})
+        if user is None:
+            return None
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
