7cbb1a92360766c7418c97b34c861be203f49c85
Ukeme Edet <ukemeedet2207@gmail.com>
2024-11-13 00:42:13 +0100

    Refactor BasicAuth class to improve user credential extraction

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -76,7 +76,11 @@ class BasicAuth(Auth):
             or ":" not in decoded_base64_authorization_header
         ):
             return (None, None)
-        return tuple(decoded_base64_authorization_header.split(":", 1))
+        i = decoded_base64_authorization_header.index(":")
+        return (
+            decoded_base64_authorization_header[:i],
+            decoded_base64_authorization_header[i + 1 :],
+        )
 
     def user_object_from_credentials(
         self, user_email: str, user_pwd: str
