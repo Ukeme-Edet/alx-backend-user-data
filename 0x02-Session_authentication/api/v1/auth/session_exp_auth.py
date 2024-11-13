0c7e5a65aadae821fadb867414bbb203f5a21995
Ukeme Edet <ukemeedet2207@gmail.com>
2024-11-13 18:28:18 +0100

    Improve session duration handling and refactor session dictionary management

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -17,7 +17,10 @@ class SessionExpAuth(SessionAuth):
         Initialize a SessionExpAuth instance
         """
         super().__init__()
-        self.session_duration = int(os.getenv("SESSION_DURATION", 0))
+        try:
+            self.session_duration = int(os.getenv("SESSION_DURATION", 0))
+        except ValueError:
+            self.session_duration = 0
 
     def creeate_session(self, user_id=None):
         """
@@ -32,8 +35,10 @@ class SessionExpAuth(SessionAuth):
         session_id = super().create_session(user_id)
         if session_id is None:
             return None
-        session_dictionary = {"user_id": user_id, "created_at": datetime.now()}
-        self.user_id_by_session_id[session_id] = session_dictionary
+        self.user_id_by_session_id[session_id] = {
+            "user_id": user_id,
+            "created_at": datetime.now(),
+        }
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None):
@@ -46,19 +51,15 @@ class SessionExpAuth(SessionAuth):
         Returns:
             str: User ID
         """
-        if (
-            session_id is None
-            or self.user_id_by_session_id.get(session_id, None) is None
-        ):
+        if session_id is None or session_id not in self.user_id_by_session_id:
             return None
+        session_dict = self.user_id_by_session_id.get(session_id)
         if self.session_duration <= 0:
-            return self.user_id_by_session_id[session_id]
+            return session_dict.get("user_id")
         if (
-            self.user_id_by_session_id[session_id].get("created_at", None)
-            is None
-            or self.user_id_by_session_id[session_id].get("created_at")
+            session_dict.get("created_at", 0)
             + timedelta(seconds=self.session_duration)
-            > datetime.now()
+            < datetime.now()
         ):
             return None
-        return self.user_id_by_session_id[session_id].get("user_id")
+        return session_dict.get("user_id")
