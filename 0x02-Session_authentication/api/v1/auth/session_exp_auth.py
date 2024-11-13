7f61fc969d62d135c5da1c3918c4bd6a9689b377
Ukeme Edet <ukemeedet2207@gmail.com>
2024-11-13 17:49:28 +0100

    Add documentation and method comments to SessionExpAuth class

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """
+SessionExpAuth module
 """
 from datetime import datetime, timedelta
 import os
@@ -7,11 +8,27 @@ from .session_auth import SessionAuth
 
 
 class SessionExpAuth(SessionAuth):
+    """
+    SessionExpAuth class
+    """
+
     def __init__(self) -> None:
+        """
+        Initialize a SessionExpAuth instance
+        """
         super().__init__()
         self.session_duration = int(os.getenv("SESSION_DURATION", 0))
 
     def creeate_session(self, user_id=None):
+        """
+        Create a new session
+
+        Args:
+            user_id: str: User ID
+
+        Returns:
+            str: Session ID
+        """
         session_id = super().create_session(user_id)
         if session_id is None:
             return None
@@ -20,6 +37,15 @@ class SessionExpAuth(SessionAuth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None):
+        """
+        Get the User ID by Session ID
+
+        Args:
+            session_id: str: Session ID
+
+        Returns:
+            str: User ID
+        """
         if (
             session_id is None
             or self.user_id_by_session_id.get(session_id, None) is None
