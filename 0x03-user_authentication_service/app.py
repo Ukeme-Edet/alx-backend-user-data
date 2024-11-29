51a6612988765bd368dc724db7a700dc20309165
Ukeme Edet <ukemeedet2207@gmail.com>
2024-11-29 21:37:11 +0100

    Add endpoint to generate reset password token for user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -97,5 +97,24 @@ def profile() -> str:
     return jsonify({"email": user.email}), 200
 
 
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token() -> str:
+    """
+    POST /reset_password
+
+    Form data:
+        - email
+
+    Returns:
+        str: The reset password token
+    """
+    data = request.form.to_dict()
+    try:
+        reset_token = AUTH.get_reset_password_token(data["email"])
+        return jsonify({"email": data["email"], "reset_token": reset_token})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
