9475a62521587b9a9fd6cc72ddacc31cbcf51099
Ukeme Edet <ukemeedet2207@gmail.com>
2024-11-29 19:14:04 +0100

    Add user registration method to Auth class with email uniqueness check

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,30 @@
 Auth module
 """
 from bcrypt import hashpw, gensalt
+from db import DB, User
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Register a new user
+
+        Args:
+            email (str): The user's email
+            password (str): The user's password
+
+        Returns:
+            User: The newly created User object
+        """
+        user = self._db.find_user_by(email=email)
+        if user:
+            raise ValueError(f"User {email} already exists")
+        return self._db.add_user(email, _hash_password(password))
 
 
 def _hash_password(password: str) -> str:
